dc2fcbace379f2e9eeb221b4e8b4eece9edafc03
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-21 10:31:53 +0100

    typing props in react typescript

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
-type GreetProps = {
-    name: string
+type GreetProp = {
+  name: string
 }
-export const Greet = (props: GreetProps) => {
+
+export const Greet = (props: GreetProp) => {
   return (
     <div>
         <h2>Welcome {props.name} You have 10 unread messages</h2>
