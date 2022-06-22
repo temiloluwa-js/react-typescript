a4d2a0f7f4ebbebcdd992e16b67911f298d203d3
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-06-22 06:15:11 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,17 @@
 import React from "react";
 import "./App.css";
-import { Greet } from "./components/Greet";
-import Heading from "./components/Heading";
-import { Oscar } from "./components/Oscar";
-import Person from "./components/Person";
-import PersonList from "./components/PersonList";
-import Status from "./components/Status";
+import { Button } from "./components/Button";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne",
-  };
 
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Diana",
-      last: "Price",
-    },
-  ];
-
-  const newNames = [
-    "Hello", "Hola", "Bora Bora"
-  ]
-
+  // const handleClick = () => {
+  //   console.log('Button clicked')
+  // }
   return (
     <div className="App">
-      {/* <Heading>Placeholder text</Heading> */}
-      <Oscar>
-        <Heading>Oscar goes to Leonardo DiCaprio</Heading>
-      </Oscar>
-      <Greet name="Vishwas"  isLoggedIn={false} />
-      {/* <Person name={personName} />
-      <PersonList names={newNames}/>
-      <Status status="loading"/> */}
+
+      <Button handleClick={(event) => {console.log('Button clicked', event)}}/>
+
     </div>
   );
 }
