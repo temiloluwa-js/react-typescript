61e261438e208c2ab798fb0f69ee383263b53124
Temiloluwa Adeleye <temiloluwagithub@gmail.com>
2022-07-21 06:33:51 +0100

    user future reference

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -5,6 +5,13 @@ type AuthUser = {
 };
 
 export const User = () => {
+  /**When type inference does not work due to the fact that
+   *  you cannot specify a future value, 
+   * you can use angle brackets (<>)
+   *  to specify what is going to be
+   *  in the state when a user fills 
+   * in something */
+  
   const [user, setUser] = useState<AuthUser | null>(null);
   const handleLogin = () => {
     setUser({
@@ -13,14 +20,16 @@ export const User = () => {
     });
   };
   const handleLogOut = () => {
-    setUser(null)
+    setUser(null);
   };
 
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
       <button onClick={handleLogOut}>Log Out</button>
-      <p>User name is: {user?.name}, User email is: {user?.email}</p>
+      <p>
+        User name is: {user?.name}, User email is: {user?.email}
+      </p>
     </div>
   );
 };
